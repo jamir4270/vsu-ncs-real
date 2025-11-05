9ddb8dd5b7e590a836e921aef53451b5a56d37a7
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-11-05 22:43:56 +0800

    Added basic student, faculty, and admin pages

---
+++ app/protected/faculty/page.tsx
@@ -0,0 +1,3 @@
+export default function Faculty() {
+  return <div>This is the faculty page.</div>;
+}
---
+++ app/protected/student/page.tsx
@@ -0,0 +1,3 @@
+export default function Student() {
+  return <div>This is the student page.</div>;
+}
