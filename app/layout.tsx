7089bab959a15f2a9f037f0e0a4575a7e78b0300
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-11-06 09:30:33 +0800

    Added layout.tsx to protected subdirectories

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
-          enableSystem
+          enableSystem={false}
           disableTransitionOnChange
         >
           {children}
