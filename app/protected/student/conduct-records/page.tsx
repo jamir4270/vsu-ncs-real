20e7ea31ee0fbfda3f1ce2eaa6d939ffffa21691
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-11-07 13:29:59 +0800

    Added conduct records table

--- app/protected/student/conduct-records/page.tsx
+++ app/protected/student/conduct-records/page.tsx
@@ -1,3 +1,49 @@
-export default function ConductRecords() {
-  return <div>This is student conduct records</div>;
+import { columns, Record } from "./columns";
+import { createClient } from "@/lib/supabase/server";
+import { DataTable } from "./data-table";
+
+async function getData(): Promise<Record[]> {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const { data: records } = await supabase
+    .from("conduct_reports")
+    .select("*, staff_profiles (full_name, title)")
+    .eq("student_id", user?.id)
+    .order("created_at", { ascending: false });
+
+  const data: Record[] =
+    records?.map((record) => ({
+      id: record.id,
+      date: record.created_at,
+      reporter: `${record.staff_profiles.title} ${record.staff_profiles.full_name}`,
+      sanction_days: record.sanction_days,
+      description: record.description,
+      is_serious_infraction: record.is_serious_infraction,
+      type: record.is_serious_infraction
+        ? "Serious Infraction"
+        : record.sanction_days > 0
+        ? "Demerit"
+        : "Merit",
+    })) || [];
+
+  return data;
+}
+export default async function ConductRecords() {
+  const data = await getData();
+  return (
+    <div className="flex flex-col w-full p-8 gap-5">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-[#0A58A3] text-2xl">My Conduct History</h1>
+        <p className="text-[#6C757D]">
+          Complete record of all merits, demerits, and serious infractions.
+        </p>
+      </div>
+      <div className="container mx-auto p-4 bg-white rounded-2xl">
+        <DataTable columns={columns} data={data} />
+      </div>
+    </div>
+  );
 }
