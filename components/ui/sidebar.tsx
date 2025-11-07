f20a4c1d65d3bd1cc9ad243abb34d97673ef73e9
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-11-07 14:27:10 +0800

    Fix: fixed sidebar issues

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -187,7 +187,7 @@ function Sidebar({
           data-sidebar="sidebar"
           data-slot="sidebar"
           data-mobile="true"
-          className="bg-inherit text-sidebar-foreground w-(--sidebar-width) p-0 [&>button]:hidden z-50"
+          className="bg-inherit text-sidebar-foreground w-(--sidebar-width) p-0 [&>button]:hidden"
           style={
             {
               "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
