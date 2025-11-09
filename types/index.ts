3263fac2105e3be4dc6a3b86d8ed2fd7ef9f52eb
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-11-09 13:14:34 +0800

    Added data models (types) in index.ts

--- types/index.ts
+++ types/index.ts
@@ -5,3 +5,54 @@ export interface SidebarProps {
   url: string;
   icon: React.ComponentType<LucideProps>;
 }
+
+export type StudentProfile = {
+  id: string;
+  full_name: string;
+  student_id: string;
+  year_level: number;
+  sex: string;
+};
+
+export type StaffProfile = {
+  id: string;
+  full_name: string;
+  employee_id: string;
+  title: string;
+  sex: string;
+};
+
+export type ConductReport = {
+  id: string;
+  created_at: string;
+  student_id: string;
+  faculty_id: string;
+  description: string;
+  is_serious_infraction: boolean;
+  sanction_days: string;
+  sanction_other: string | null;
+};
+
+export type ConductReportWithReporter = ConductReport & {
+  staff_profiles: Pick<StaffProfile, "full_name" | "title"> | null;
+};
+
+export type StudentConductRecord = {
+  id: string;
+  date: string;
+  reporter: string;
+  description: string;
+  sanction_days: number;
+  is_serious_infraction: boolean;
+  type: "Merit" | "Demerit" | "Serious Infraction";
+  sanction_other: string | null;
+};
+
+export type FacultyStudentRecord = {
+  id: string;
+  full_name: string;
+  student_id: string;
+  year_level: number;
+  sex: string;
+  net_sanction: number;
+};
