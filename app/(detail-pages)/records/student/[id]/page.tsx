a8cecccdf27d7ae45f5a38f4a8f507f4b5bbdaee
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-11-10 14:06:52 +0800

    Added personal information card

--- app/(detail-pages)/records/student/[id]/page.tsx
+++ app/(detail-pages)/records/student/[id]/page.tsx
@@ -1,12 +1,15 @@
-import {
-  fetchStudentConductRecords,
-  fetchStudentConductRecordsWithReporter,
-  fetchStudentProfile,
-} from "@/lib/data";
+import { fetchStudentConductRecords, fetchStudentProfile } from "@/lib/data";
 import { notFound } from "next/navigation";
 import { columns } from "../../_components/conduct-records-column";
 import { DataTable } from "../../_components/conduct-records-table";
-import { StudentConductRecord } from "@/types";
+import ConductCardList from "../../_components/conduct-card-list";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default async function StudentRecordPage({
   params,
@@ -20,18 +23,45 @@ export default async function StudentRecordPage({
     notFound();
   }
 
-  const studentProfile = await fetchStudentProfile(studentID);
+  const student = await fetchStudentProfile(studentID);
   const conductRecords = await fetchStudentConductRecords(studentID);
 
   return (
-    <div>
-      <div>Profile Basic Information Here</div>
+    <div className="flex flex-col p-10 gap-5">
+      <div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Student Information</CardTitle>
+            <CardDescription>
+              Personal details and academic details
+            </CardDescription>
+            <CardContent className="flex flex-row flex-wrap px-0 pt-4 justify-between ">
+              <div className="flex-1">
+                <p>Full Name</p>
+                <h1>{student.full_name}</h1>
+              </div>
+              <div className="flex-1">
+                <p>Student ID</p>
+                <h1>{student.student_id}</h1>
+              </div>
+              <div className="flex-1">
+                <p>Year Level</p>
+                <h1>{student.year_level}</h1>
+              </div>
+              <div className="flex-1">
+                <p>Sex</p>
+                <h1>{student.sex}</h1>
+              </div>
+            </CardContent>
+          </CardHeader>
+        </Card>
+      </div>
       <div>Summary Cards here</div>
-      <div className="p-10">
-        <DataTable
-          columns={columns}
-          data={conductRecords as StudentConductRecord[]}
-        />
+      <div className="hidden md:block">
+        <DataTable columns={columns} data={conductRecords} />
+      </div>
+      <div className="md:hidden">
+        <ConductCardList data={conductRecords} />
       </div>
     </div>
   );
