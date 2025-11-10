7fc3aeb6705b8fe0ace25b3d784107d43d5c7051
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-11-10 14:45:10 +0800

    Added styling to student data table

--- app/(main-pages)/protected/student/conduct-records/page.tsx
+++ app/(main-pages)/protected/student/conduct-records/page.tsx
@@ -18,7 +18,7 @@ export default async function ConductRecords() {
           Complete record of all merits, demerits, and serious infractions.
         </p>
       </div>
-      <div className="hidden md:block container mx-auto p-4 bg-white rounded-2xl">
+      <div className="hidden md:block container mx-auto p-4">
         <DataTable columns={columns} data={data} />
       </div>
       <div className="md:hidden">
