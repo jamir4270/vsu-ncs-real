8b2bd2501863932975a9f1d1f0dc5286b4c15368
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-11-15 05:28:49 +0800

    Imported parseName function to report-card-list component

--- app/(main-pages)/protected/faculty/reports/_components/report-card-list.tsx
+++ app/(main-pages)/protected/faculty/reports/_components/report-card-list.tsx
@@ -14,6 +14,7 @@ import {
 
 import { ChevronDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { parseName } from "@/lib/utils";
 
 interface StudentProfile {
   student_id: string;
