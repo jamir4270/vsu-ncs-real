9222a34de1bc12a682d58ee5b60e10e9410a8c43
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-11-14 21:39:07 +0800

    Updated search filter logic in faculty management page

--- app/(main-pages)/protected/admin/faculty-management/_components/data-table.tsx
+++ app/(main-pages)/protected/admin/faculty-management/_components/data-table.tsx
@@ -85,9 +85,11 @@ export function DataTable<TData, TValue>({
       const idMatch = profile.employee_id
         .toLowerCase()
         .includes(lowerCaseQuery);
-      const nameMatch = profile.full_name
-        .toLowerCase()
-        .includes(lowerCaseQuery);
+      const nameMatch =
+        profile.first_name.toLowerCase().includes(lowerCaseQuery) ||
+        profile.middle_name.toLowerCase().includes(lowerCaseQuery) ||
+        profile.last_name.toLowerCase().includes(lowerCaseQuery) ||
+        profile.suffix.toLowerCase().includes(lowerCaseQuery);
 
       return idMatch || nameMatch;
     });
