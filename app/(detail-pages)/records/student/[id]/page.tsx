ad45c906c7fa7203cc131c210d9f6a221ac8ca32
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-11-14 21:53:10 +0800

    Updated name parsing in student detail page

--- app/(detail-pages)/records/student/[id]/page.tsx
+++ app/(detail-pages)/records/student/[id]/page.tsx
@@ -13,6 +13,8 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { StudentConductRecord } from "@/types";
+import { parseName } from "@/lib/utils";
+import { profile } from "console";
 
 export default async function StudentRecordPage({
   params,
@@ -48,6 +50,13 @@ export default async function StudentRecordPage({
   const totalDemerits = sanctionCount.demerit;
   const totalMerits = Math.abs(sanctionCount.merit);
 
+  const full_name = parseName(
+    student.first_name,
+    student.middle_name,
+    student.last_name,
+    student.suffix
+  );
+
   return (
     <div className="flex flex-col p-10 gap-5">
       <div>
@@ -58,7 +67,7 @@ export default async function StudentRecordPage({
               Personal details and academic details
             </CardDescription>
             <CardContent className="flex flex-col md:flex-row px-0 pt-4 justify-between ">
-              <InfoCard title={"Full Name"} description={student.full_name} />
+              <InfoCard title={"Full Name"} description={full_name} />
               <InfoCard title={"Student ID"} description={student.student_id} />
               <InfoCard title={"Year Level"} description={student.year_level} />
               <InfoCard title={"Sex"} description={student.sex} />
