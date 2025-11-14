1def7acdc9dd124977df03a897788d2e16a1c7c8
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-11-14 15:43:40 +0800

    Created function to parse name

--- lib/data.ts
+++ lib/data.ts
@@ -319,4 +319,15 @@ export async function fetchDefaultStudentList() {
   }
 }
 
-
+export function parseName(
+  first_name: string,
+  middle_name: string,
+  last_name: string,
+  suffix: string
+) {
+  let full_name = `${first_name} ${middle_name
+    .charAt(0)
+    .toUpperCase()}. ${last_name} ${suffix}`;
+  full_name = full_name.trim().replace(/\s+/g, " ");
+  return full_name;
+}
